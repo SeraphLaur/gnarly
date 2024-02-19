c8713c5128b5f938817b61485ed41e438d6d894c
SeraphLaur <laurence.arcila@gmail.com>
2024-02-19 12:49:19 +0800

    push

--- src/Product.js
+++ src/Product.js
@@ -1,70 +1,103 @@
 import Shirts from "./Shirts";
-import Sold from "./Sold";
 const shirtslist = [
   {
     name: "GRAV GLIDER",
     price: "₱550",
     origPrice: "₱650",
+    quantity: 5,
+    isFeatured: false,
     img: "gravglider.png",
+    altimg: "altgravglider.png",
   },
   {
     name: "CIRCUIT BUZZ",
     price: "₱550",
     origPrice: "₱650",
+    quantity: 5,
+    isFeatured: false,
     img: "circuitbuzz.png",
+    altimg: "altcircuitbuzz.png",
   },
   {
     name: "BUZZ CIRCUIT",
     price: "₱550",
     origPrice: "₱650",
+    quantity: 5,
+    isFeatured: true,
     img: "buzzcircuit.png",
+    altimg: "altbuzzcircuit.png",
   },
   {
     name: "PILLAR",
     price: "₱550",
     origPrice: "₱650",
+    quantity: 5,
+    isFeatured: false,
     img: "pillar.png",
+    altimg: "altpillar.png",
   },
   {
     name: "SCANNED",
     price: "₱630",
+    isFeatured: true,
     origPrice: "₱750",
+    quantity: 5,
     img: "scanned.png",
+    altimg: "altscanned.png",
   },
   {
     name: "WORRY FREE",
     price: "₱550",
     origPrice: "₱650",
+    quantity: 5,
+    isFeatured: false,
     img: "worryfree.png",
+    altimg: "altworryfree.png",
   },
   {
     name: "SS SHIFTY",
     price: "₱720",
     origPrice: "₱850",
+    quantity: 5,
+    isFeatured: false,
     img: "ssshifty.png",
+    altimg: "altssshifty.png",
   },
   {
     name: "YAM L/S",
     price: "₱1050",
     origPrice: "₱1250",
+    quantity: 5,
+    isFeatured: true,
     img: "yamls.png",
+    altimg: "yamls.png",
   },
   {
     name: "PINE GREEN L/S",
     price: "₱1,050",
     origPrice: "₱1,250",
+    quantity: 0,
+    isFeatured: true,
     img: "pinegreenls.png",
+    altimg: "pinegreenls.png",
   },
-];
-
-const soldlist = [
   {
     name: "SWEEP EMBRO",
+    price: "₱1,050",
+    origPrice: "₱1,250",
+    quantity: 0,
+    isFeatured: false,
     img: "sweepembro.png",
+    altimg: "sweepembro.png",
   },
   {
     name: "RIFF OUT",
+    origPrice: "₱1,250",
+    origPrice: "₱1,250",
+    quantity: 0,
+    isFeatured: true,
     img: "riffout.png",
+    altimg: "riffout.png",
   },
 ];
 
@@ -90,9 +123,6 @@ function Product() {
       <div class="row px-5">
         {shirtslist.map((shirts) => (
           <Shirts key={shirts.name} shirtsss={shirts} />
-        ))}
-        {soldlist.map((shirts) => (
-          <Sold key={shirts.name} shirtsss={shirts} />
         ))}
       </div>
     </div>
--- src/Shirts.js
+++ src/Shirts.js
@@ -1,16 +1,39 @@
+import React, { useState } from "react";
+
 function Shirts(props) {
+  const [showAltImage, setShowAltImage] = useState(false);
+
   return (
     <div className="col-3 mt-3">
-      <div class="card card-color border-5 rounded-0 text-center">
-        <img class="card-img-top" src={props.shirtsss.img} alt="shirt" />
-        <div class="card-body card-color text-white">
-          <div class="card-title card-title-font">{props.shirtsss.name}</div>
-          <div class="card-text">
-            <span className="card-title-price">{props.shirtsss.price}</span>
-            <span className="card-title-orig">
-              <strike>{props.shirtsss.origPrice}</strike>
-            </span>
+      <div className="card card-color border-5 rounded-0 text-center">
+        <img
+          className={`card-img-top ${showAltImage ? "card-img-alt" : ""}`}
+          src={showAltImage ? props.shirtsss.altimg : props.shirtsss.img}
+          alt="shirt"
+          onMouseEnter={() => setShowAltImage(true)}
+          onMouseLeave={() => setShowAltImage(false)}
+        />
+        <div
+          className={`card-body card-color text-white ${
+            showAltImage ? "hovered" : ""
+          }`}
+        >
+          <div className="card-title card-title-font">
+            {props.shirtsss.name}
           </div>
+
+          {props.shirtsss.quantity === 0 ? (
+            <div className="card-text">
+              <span className="card-sold">SOLD OUT</span>
+            </div>
+          ) : (
+            <div className="card-text">
+              <span className="card-title-price">{props.shirtsss.price}</span>
+              <span className="card-title-orig">
+                <strike>{props.shirtsss.origPrice}</strike>
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
